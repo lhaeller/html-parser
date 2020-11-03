b80e18cecebc1a74e370aefe17bcb7d6f9b6bd2d
unknown <lhaeller@tuta.io>
2020-11-03 10:54:49 +0100

    method of choice

--- src/__main__.py
+++ src/__main__.py
@@ -5,20 +5,7 @@
 
 path_to_test_file = "../data/data_in/2020-11-03.txt"
 
-# txt_file = open("../data/data_in/2020-11-03.txt","r")
-# print("")
-# print("- - - - - - - - - - - - ")
-# print("U S I N G  read()-san")
-# print(txt_file.read())
-# txt_file.close()
-
 # TODO: read each line and save to object
-# with open(path_to_test_file) as test_file:
-#     print("")
-#     print("- - - - - - - - - - - - ")
-#     print("U S I N G  I T E R A T O R")
-#     for line in test_file:
-#         print(line)
 
 with open(path_to_test_file) as test_file:
     print("")
