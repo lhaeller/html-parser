b7251d31136a518c9638e1fcc1c3fe0e03cad8cb
unknown <lhaeller@tuta.io>
2020-11-03 10:53:42 +0100

    no more newline bug

--- src/__main__.py
+++ src/__main__.py
@@ -2,30 +2,32 @@
 
 # TODO: read TXT file
 # TODO: read all TXT files in folder "data_in"
-txt_file = open("../data/data_in/2020-11-03.txt","r")
+
 path_to_test_file = "../data/data_in/2020-11-03.txt"
 
-print("")
-print("- - - - - - - - - - - - ")
-print("U S I N G  read()-san")
-print(txt_file.read())
-
-txt_file.close()
+# txt_file = open("../data/data_in/2020-11-03.txt","r")
+# print("")
+# print("- - - - - - - - - - - - ")
+# print("U S I N G  read()-san")
+# print(txt_file.read())
+# txt_file.close()
 
 # TODO: read each line and save to object
-with open(path_to_test_file) as test_file:
-    print("")
-    print("- - - - - - - - - - - - ")
-    print("U S I N G  I T E R A T O R")
-    for line in test_file:
-        print(line)
+# with open(path_to_test_file) as test_file:
+#     print("")
+#     print("- - - - - - - - - - - - ")
+#     print("U S I N G  I T E R A T O R")
+#     for line in test_file:
+#         print(line)
 
 with open(path_to_test_file) as test_file:
     print("")
     print("- - - - - - - - - - - - ")
     print("U S I N G   readlines()-san")
+    line_count = 0
     for line in test_file.readlines():
-        print(line)
-
+        # if not "\n" in line:
+        print(line_count, line.strip())
+        line_count += 1
 
 # TODO: define parse rules (.TXT to .HTML)
